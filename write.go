906ac0c827ecba5dd8175cec67ac7fe66444803b
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-02-02 23:22:22 -0800

    don't override the "Write" function name

--- write.go
+++ write.go
@@ -25,12 +25,25 @@ type WriteFlusher interface {
 	http.Flusher
 }
 
-func Write(res WriteFlusher, buf *bytes.Buffer, msgNumber int, event EventName, data string) (int, error) {
+func Send(res WriteFlusher, buf *bytes.Buffer, msgNumber int, event EventName, data string) (int, error) {
 	if buf == nil {
 		buf = bytes.NewBuffer(make([]byte, 0, 1024))
 	} else {
 		buf.Reset()
 	}
+	if msgNumber <= 0 {
+		msgNumber = 1
+	}
+	writeMessageString(buf, msgNumber, event, data)
+	n, err := io.Copy(res, buf)
+	if err != nil {
+		return int(n), err
+	}
+	res.Flush()
+	return int(n), nil
+}
+
+func writeMessageString(buf *bytes.Buffer, msgNumber int, event EventName, data string) {
 	_, _ = buf.WriteString("id: ")
 	_, _ = buf.WriteString(strconv.Itoa(msgNumber))
 	_, _ = buf.WriteString("\n")
@@ -40,10 +53,4 @@ func Write(res WriteFlusher, buf *bytes.Buffer, msgNumber int, event EventName, 
 	_, _ = buf.WriteString("data: ")
 	_, _ = buf.WriteString(data)
 	_, _ = buf.WriteString("\n\n")
-	n, err := io.Copy(res, buf)
-	if err != nil {
-		return int(n), err
-	}
-	res.Flush()
-	return int(n), nil
 }
